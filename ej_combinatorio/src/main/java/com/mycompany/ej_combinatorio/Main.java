731886da6ab224c54a05465cc36dc25a0aa4d4c6
lucianogatti <81379031+lucianogatti@users.noreply.github.com>
2021-08-31 11:15:43 -0300

    ejercicio

---
+++ ej_combinatorio/src/main/java/com/mycompany/ej_combinatorio/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ej_combinatorio;
+
+/**
+ *
+ * @author Luciano
+ */
+public class Main {
+    public static void main(String[] args) {
+        double operacion;
+        
+        operacion = ((6.0/5.0)-(2.0/3.0)*(7.0/2.0)+(2.0/30.0))/((1.0/3.0)/5.0);
+        
+        System.out.println("El resulatdo es: "+operacion);
+    }
+}
